0f134eefa19350b116a90de4380c78f614097325
Ygor Santana <ygor.ys@gmail.com>
2021-03-15 22:49:23 -0300

    Passing all tests

--- semana6/BancoNapp/contas/ContaPessoaJuridica.py
+++ semana6/BancoNapp/contas/ContaPessoaJuridica.py
@@ -2,4 +2,36 @@ from BancoNapp.contas.Conta import Conta
 
 
 class ContaPessoaJuridica(Conta):
-    pass
+    def __init__(self, *args, **kwargs):
+        kwargs.update({
+            'limite': kwargs.get('limite', 1500),
+        })
+        super().__init__(*args, **kwargs)
+        self.empresa = kwargs.get('empresa', None)
+
+    def deposito(self, valor):
+        if not isinstance(valor, (int, float)):
+            raise TypeError('O depósito precisa ser numérico')
+        if valor <= 0:
+            raise ValueError('Valor do depósito precisa ser maior que zero')
+        self.saldo += valor
+        self.extrato.append(('D', valor))
+
+    def saque(self, valor):
+        if not isinstance(valor, (int, float)):
+            raise TypeError('O valor do saque precisa ser numérico')
+        saque = valor
+        if self.saldo < valor:
+            _valor = valor - self.saldo
+            if self.limite < valor:
+                raise ValueError('Valor do saque supera seu saldo e seu limite')
+            self.limite -= _valor
+            self.saldo -= valor
+        else:
+            self.saldo -= valor
+
+        self.extrato.append(('S', valor))
+        return saque
+    
+    def get_extrato(self):
+        return self.extrato
--- semana6/BancoNapp/contas/ContaPoupanca.py
+++ semana6/BancoNapp/contas/ContaPoupanca.py
@@ -2,4 +2,39 @@ from BancoNapp.contas.Conta import Conta
 
 
 class ContaPoupanca(Conta):
-   pass
+   def __init__(self, *args, **kwargs):
+      kwargs.update({
+         'limite': kwargs.get('limite', 0),
+         'saldo': kwargs.get('saldo', 0),
+      })
+      super().__init__(*args, **kwargs)
+      self.profissao = kwargs.get('profissao', '')
+
+   def deposito(self, valor):
+      if not isinstance(valor, (int, float)):
+         raise TypeError('O depósito precisa ser numérico')
+
+      if valor <= 0:
+         raise ValueError('Valor do depósito precisa ser maior que zero')
+
+      self.extrato.append(('D', valor))
+      self.saldo += valor
+
+   def saque(self, valor):
+      if not isinstance(valor, (int, float)):
+         raise TypeError('O valor do saque precisa ser numérico')
+      if self.saldo < valor:
+         raise ValueError('Valor do saque supera seu saldo.')
+   
+      self.saldo -= valor
+      self.extrato.append(('S', valor))
+      return valor
+
+   def get_extrato(self):
+      return self.extrato
+
+   def rendimento_aniversario(self, juros):
+      if juros < 0 or juros > 1:
+         raise ValueError('Os juros precisam ser entre 0 (0%) e 1 (100%).')
+
+      self.saldo *= 1 + juros
